8b38de467710abe9a41d05669d031b167f4b0df2
mattspeidel <mattspeidel@gmail.com>
2019-06-03 16:12:23 -0400

    further debugging

--- house-hunting-hard.py
+++ house-hunting-hard.py
@@ -1,15 +1,16 @@
 annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
-r = float(input("Enter the expected annual rate of return [0.04]: ")) or float(0.04)
+r = input("Enter the expected annual rate of return [0.04]: ") or 0.04
 total_cost = float(input("Enter the cost of your dream home: "))
-portion_down_payment = float(input("Enter the percent of your home's cost to save as a down payment [0.25]: ")) or float(0.25)
+portion_down_payment = input("Enter the percent of your home's cost to save as a down payment [0.25]: ") or 0.25
 monthly_salary = annual_salary / 12
 monthly_savings = monthly_salary * portion_saved
 current_savings = 0.0
 months = 0
+monthly_r = float(r) / 12
 
-while current_savings < (total_cost * portion_down_payment):
-    current_savings = current_savings + (current_savings * (r / 12)) + monthly_savings
+while current_savings < (total_cost * float(portion_down_payment)):
+    current_savings = current_savings + (current_savings * monthly_r) + monthly_savings
     months = months + 1
 
 print("Number of months: " + str(months))
